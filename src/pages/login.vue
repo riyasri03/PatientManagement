c1c45db1f23097cb52b55668365706eb0a8258e2
Riya Srivastava <riyasrivastava@Riyas-MacBook-Pro.local>
2021-03-02 21:39:01 +0530

    login done

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,12 +1,18 @@
 <template>
-<div id='login'>
+<div id='login' class='bg-img'>
   <navbar />
+        <form action="" class="container">
         <h1>Login</h1>
-        <input type='text' name='username' v-model='input.username' placeholder="Username" />
+        <label for="email"><b>Email</b></label>
+        <br>
+        <input type='text' name='username' v-model='input.username' placeholder="Username" id="" />
+        <br>
+        <label for="psw"><b>Password</b></label>
         <br>
         <input type='password' name='password' v-model='input.password' placeholder="Password" />
         <br>
         <button type='button' v-on:click='login()'>Login</button>
+        </form>
     </div>
 </template>
 
@@ -51,5 +57,37 @@ export default {
         margin-top: 200px;
         padding: 20px;
     } */
+.bg-img {
+  /* The image used */
+  background-image: url("https://www.apollohospitals.com/images/patient-care/speak-patient.jpg");
+
+  min-height: 800px;
+
+  /* Center and scale the image nicely */
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  position: relative;
+}
+input[type=text], input[type=password] {
+  width: auto;
+  padding: 15px;
+  margin: 5px 0 22px 0;
+  border: none;
+  background: #f1f1f1;
+}
+
+input[type=text]:focus, input[type=password]:focus {
+  background-color: #ddd;
+  outline: none;
+}
+.container {
+  position: absolute;
+  right: 90px;
+  margin: 50px;
+  max-width: 500px;
+  padding: 20px;
+  background-color: white;
+}
 </style>
 More
--- src/pages/registration.vue
+++ src/pages/registration.vue
@@ -3,30 +3,66 @@
     <navbar />
     <form @submit.prevent="login" class= "form">
       <div>
-        <label for="username">username</label>
-        <input name="username" v-model="username" placeholder="username">
+        <label for="Name">Name</label>
+        <input name="Name" v-model="Name" placeholder="Name">
+      </div>
+      <div>
+        <label for="email">Email</label>
+        <input name="email" v-model="email" placeholder="Email">
       </div>
       <div>
         <label for="password">password</label>
         <input name="password" v-model="password" placeholder="password" type="password">
       </div>
       <div>
-        <label for="firstName">first name</label>
-        <input name="firstName" v-model="firstName" placeholder="first name">
-      </div>
+        <label for="gender">Gender</label>
+        <br>
+      <input type="radio" id="male" value="male" v-model="gender">
+        <label for="male">Male</label>
+        <br>
+        <input type="radio" id="female" value="female" v-model="gender">
+        <label for="female">Female</label>
+        <br>
+         <input type="radio" id="others" value="others" v-model="gender">
+        <label for="others">Others</label>
+     </div>
       <div>
-        <label for="lastName">last name</label>
-        <input name="lastName" v-model="lastName" placeholder="last name">
-      </div>
-<div>
-        <label for="age">age</label>
+        <label for="age">Age</label>
         <input name="age" v-model="age" placeholder="age" type="number">
       </div>
       <div>
-        <label for="address">address</label>
+        <label for="address">Address</label>
         <input name="address" v-model="address" placeholder="address">
       </div>
+      <div>
+        <label for="contact">Contact</label>
+        <input name="contact" v-model="contact" placeholder="contact">
+      </div>
+      <div>
+        <label for="emcontact">Emergency Contact</label>
+        <input name="emcontact" v-model="emcontact" placeholder="Emergency contact">
+      </div>
+      <div>
+        <label for="admissionmonth">Admission Month</label>
+        <input name="admissionmonth" v-model="admissionmonth" placeholder="admission month">
+      </div>
+      <div>
+        <label for="height">Height</label>
+        <input name="height" v-model="height" placeholder="Height">
+      </div>
+      <div>
+        <label for="weight">Weight</label>
+        <input name="weight" v-model="weight" placeholder="Weight">
+      </div>
+      <div>
+        <label for="currmed">Current Medication</label>
+        <input name="currmed" v-model="currmed" placeholder="current medication">
+      </div>
+      <br>
       <input type="submit" value="register">
+      <div class="container signin">
+         <p>Already have an account? <a href='/login'>Login in</a>.</p>
+      </div>
     </form>
   </div>
 </template>
@@ -39,17 +75,23 @@ export default {
   },
   data () {
     return {
-      username: '',
+      Name: '',
+      email: '',
       password: '',
-      firstName: '',
-      lastName: '',
+      gender: [],
       age: '',
-      address: ''
+      address: '',
+      contact: '',
+      emcontact: '',
+      admissionmonth: '',
+      height: '',
+      weight: '',
+      currmed: ''
     }
   },
   methods: {
     async login () {
-      const { username, password, firstName, lastName, age, address } = this
+      const { Name, email, password, gender, age, address, contact, emcontact, admissionmonth, height, weight, currmed } = this
       const res = await fetch(
         'https://SomberHandsomePhysics--five-nine.repl.co/register',
         {
@@ -58,12 +100,18 @@ export default {
             'Content-Type': 'application/json'
           },
           body: JSON.stringify({
-            username,
+            Name,
+            email,
             password,
-            firstName,
-            lastName,
+            gender,
             age,
-            address
+            address,
+            contact,
+            emcontact,
+            admissionmonth,
+            height,
+            weight,
+            currmed
           })
         }
       )
