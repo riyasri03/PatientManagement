4491081a5336671c9fa86e213777275c184cb66e
Riya Srivastava <riyasrivastava@Riyas-MacBook-Pro.local>
2021-03-08 12:20:19 +0530

    final project done

--- src/pages/payment.vue
+++ src/pages/payment.vue
@@ -1,5 +1,6 @@
 <template>
-  <div><h1>Payment Successful!!</h1></div>
+  <div><h1>Payment Successful!!</h1>
+  <router-link to ="/home"><button type='button' class=" btn payment"><i class="fa fa-sign-out"></i> Go To Home</button></router-link></div>
 </template>
 
 <script>
